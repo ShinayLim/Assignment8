f4534b8fdd34f1b8c71369292cff1f2e1488c2bd
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-12-08 23:03:55 +0800

    greater than or less than

--- guessing_game.py
+++ guessing_game.py
@@ -17,3 +17,18 @@ option = int(option)
 if option ==1:
     print("You have to guess the number that is between 0 to 100")
     print("You only have three(3) tries")
+
+    guess = input("Guess a number [0-100]: ")
+    guess = int(guess)
+    tries += 1
+
+    if guess > number:
+        print("Guess less than that number...")
+    
+    if guess < number:
+        print("Guess greater than that number...")
+
+    while guess != number and tries < 3:
+        guess = input("Try again: ")
+        guess = int(guess)
+        tries += 1
