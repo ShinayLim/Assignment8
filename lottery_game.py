0327e5be33cc48748e9e63b14a3879d54ca77b80
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-12-08 22:34:34 +0800

    main game

--- lottery_game.py
+++ lottery_game.py
@@ -1,1 +1,25 @@
 import random
+
+def main_game():
+    winning_number1 = False 
+    winning_number2 = False
+    winning_number3 = False
+
+    lotto_number1 = random.randint(0,9) 
+    lotto_number2 = random.randint(0,9)
+    lotto_number3 = random.randint(0,9)
+
+    number1 = int(input("First lucky number between 0 to 9: "))
+    number2 = int(input("Second lucky number between 0 to 9: "))
+    number3 = int(input("Third lucky number between 0 to 9: "))
+
+    if number1 == lotto_number1 or number1 == lotto_number2 or number1 == lotto_number3:
+        winning_number1 = True 
+    if number2 == lotto_number1 or number2 == lotto_number2 or number2 == lotto_number3:
+        winning_number2 = True 
+    if number3 == lotto_number1 or number3 == lotto_number2 or number3 == lotto_number3:
+        winning_number3 = True 
+    if winning_number1 == True and winning_number2 == True and winning_number3 == True:
+        print("Winner!")
+    else:
+        print("You loss.")
